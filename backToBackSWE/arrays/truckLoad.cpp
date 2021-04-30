2b84755f0d8dbd5d7c28d4fab41d50260686b352
diru1100 <rohitdandamudi.1100@gmail.com>
2021-04-30 17:49:40 +0530

    Add truckLoad solution in python

---
+++ backToBackSWE/arrays/truckLoad.cpp
@@ -0,0 +1,33 @@
+#include<stdc++.h>
+
+using namespace std;
+
+int main(){
+
+    int truckSpace = 90;
+    truckSpace -= 30;
+
+    int numPackages = 5;
+
+    vector<int> packageSpace[20,30,35,35,60];
+
+    unordered_map<int, int> spaceToIndex;
+
+    for (int i =0; i < numPackages; i++ ){
+
+        int val = packageSpace[i];
+
+        if(spaceToIndex[truckSpace-val]){
+
+
+        }
+        if
+        spaceToIndex[truckSpace-val] =
+        }
+
+
+        if 
+    }
+
+    return 0;
+}
